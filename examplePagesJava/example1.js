a082d016e49262c344873a7ab88b3b0489f78cea
LordVeggie <35328638+LordVeggie@users.noreply.github.com>
2021-11-24 17:14:16 +0200

    example 1 loginPage with moving text

--- examplePagesJava/example1.js
+++ examplePagesJava/example1.js
@@ -1,1 +1,7 @@
 const page = document.querySelector('#example1');
+const labels = document.querySelectorAll('.textBoxContainer label');
+
+labels.forEach(label =>
+    {
+        label.innerHTML = label.innerText.split('').map((letter, idx) => `<span style="transition-delay:${idx*50}ms">${letter}</span>`).join('');
+    })
